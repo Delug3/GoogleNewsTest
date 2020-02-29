ff438a03c6dd7bf36beb73aff84d8de17380a876
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-02-29 11:56:54 +0100

    Cleaned code

--- app/src/main/java/com/delug3/googlenewstest/responses/ArticlesResponse.java
+++ app/src/main/java/com/delug3/googlenewstest/responses/ArticlesResponse.java
@@ -5,8 +5,8 @@ import com.delug3.googlenewstest.models.Articles;
 import java.util.ArrayList;
 
 public class ArticlesResponse {
-    private ArrayList<Articles> articles;
 
+    private ArrayList<Articles> articles;
 
     public ArrayList<Articles> getArticles() {
         return articles;
@@ -15,5 +15,4 @@ public class ArticlesResponse {
     public void setArticles(ArrayList<Articles> articles) {
         this.articles = articles;
     }
-
 }
